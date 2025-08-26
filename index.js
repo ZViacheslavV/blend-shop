7e8633df47c3f29826b7e93c22318109c78ac361
ZVyacheslavV <vvvv.zkv@gmail.com>
2025-08-26 21:11:04 +0000

    Deploying to gh-pages from @ ZVyacheslavV/blended-products-refined@5e0ed93893edd68fcf389e51c132c4319b16de02 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{a,r as e,b as t,h as r,c as d,d as i}from"./assets/handlers-C2qAIdP8.js";import"./assets/vendor-4yCzdkXl.js";document.addEventListener("DOMContentLoaded",a);e.categoriesList.addEventListener("click",t);e.productsList.addEventListener("click",r);e.searchFormBtnClear.addEventListener("click",d);e.searchForm.addEventListener("submit",i);
+import{a,r as e,b as t,h as r,c as d,d as i}from"./assets/handlers-DWaqqXCn.js";import"./assets/vendor-4yCzdkXl.js";document.addEventListener("DOMContentLoaded",a);e.categoriesList.addEventListener("click",t);e.productsList.addEventListener("click",r);e.searchFormBtnClear.addEventListener("click",d);e.searchForm.addEventListener("submit",i);
 //# sourceMappingURL=index.js.map
--- wishlist.js
+++ wishlist.js
@@ -1,2 +1,2 @@
-import{i as t,r as e,h as i}from"./assets/handlers-C2qAIdP8.js";import"./assets/vendor-4yCzdkXl.js";document.addEventListener("DOMContentLoaded",t);e.productsList.addEventListener("click",i);
+import{i as t,r as e,h as i}from"./assets/handlers-DWaqqXCn.js";import"./assets/vendor-4yCzdkXl.js";document.addEventListener("DOMContentLoaded",t);e.productsList.addEventListener("click",i);
 //# sourceMappingURL=wishlist.js.map
